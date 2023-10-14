b2713351d13af8cc2b7db5102e864576e7dd49ca
李登淳 <mikecovlee@163.com>
2023-10-14 16:35:16 +0800

    Update init-env.sh

--- .devcontainer/init-env.sh
+++ .devcontainer/init-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt update
-apt install wget -y
+apt install wget libcurl4 -y
 wget https://github.com/covscript/csbuild/releases/download/ubuntu-schedule/covscript-amd64-nightly.deb
 dpkg -i covscript-amd64-nightly.deb
 rm covscript-amd64-nightly.deb
