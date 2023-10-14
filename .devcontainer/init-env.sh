96eba4a13b2c516bde9999acf989ccb7d901e31f
李登淳 <mikecovlee@163.com>
2023-10-14 16:23:31 +0800

    update

--- .devcontainer/init-env.sh
+++ .devcontainer/init-env.sh
@@ -4,3 +4,4 @@ apt install wget -y
 wget https://github.com/covscript/csbuild/releases/download/ubuntu-schedule/covscript-amd64-nightly.deb
 dpkg -i covscript-amd64-nightly.deb
 rm covscript-amd64-nightly.deb
+cspkg -i ecs_bootstrap netutils.csym --yes
